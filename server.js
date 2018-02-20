35aad670c1b31e54fb338eb819716273169c6f8e
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-20 20:45:29 +0100

    Add comments

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const bunyanConfig = require('./configs/bunyan');
 
+// Set port, init express
 const port = process.env.PORT || 8080;
 const app = express();
 
+// Init bunyan logger
 const log = bunyan.createLogger(bunyanConfig);
 
+// Watch for memory leaks, log leaks
 memwatch.on('leak', info => {
   log.fatal('Memory leak detected:', info);
 });
 
+// Parse json and urlencoded bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Start application
 log.info('Application started');
