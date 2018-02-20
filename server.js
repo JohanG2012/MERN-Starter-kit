3569d940ae95020052034e0f21fdc2e530bd76e8
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-20 20:21:14 +0100

    Configure bunyan and memwatch

--- server.js
+++ server.js
@@ -2,3 +2,23 @@ require('dotenv').config();
 const express = require('express');
 
 const port = process.env.PORT || 8080;
+
+const log = bunyan.createLogger({
+  name: 'integrering',
+  streams: [
+    {
+      level: 'debug',
+      stream: process.stdout // log INFO and above to stdout
+    },
+    {
+      level: 'info',
+      path: `${process.env.APP_ROOT}/logs/app.json` // log ERROR and above to a file
+    }
+  ]
+});
+
+log.info('Application started');
+
+memwatch.on('leak', info => {
+  log.fatal('Memory leak detected:\n', info);
+});
