4f43a4ec41900c44b04e9f5587799e19ed3b0bde
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-22 23:43:06 +0100

    Add project dependencies
    Add npm install, installation of nodemon, application start

--- scripts/node.sh
+++ scripts/node.sh
@@ -2,3 +2,8 @@ sudo apt-get install nodejs npm -y
 sudo npm install -g n
 sudo n latest
 sudo apt-get purge nodejs -y
+sudo npm install -g nodemon
+cd /vagrant/app
+npm install
+cd server
+nodemon --ignore 'logs' server.js -L | ../node_modules/.bin/bunyan
