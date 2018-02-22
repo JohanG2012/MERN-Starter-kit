c9b7551f99cd14218dedfb71ae78d4da5e767791
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-22 23:47:03 +0100

    Update mongo install script

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 # Installing mongo-db (http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/)
   echo "installing mongodb..."
-  sudo mkdir -p /data/db
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-  echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+  echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
   sudo apt-get update
   sudo apt-get install -y mongodb-org
   sudo service mongod start
