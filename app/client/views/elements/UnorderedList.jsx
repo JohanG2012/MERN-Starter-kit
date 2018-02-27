8981e7947e67c9dbfe93f5899a5b48161fb84bc2
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-27 18:16:32 +0100

    Add UnorderedList component

---
+++ app/client/views/elements/UnorderedList.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const UnorderedList = styled.ul`
+  zoom: 1;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  outline: none;
+  margin-bottom: 0;
+  padding-left: 0;
+  list-style: none;
+`;
+
+export default UnorderedList;
