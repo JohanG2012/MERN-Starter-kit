bae7cdd32fb44602a944cf3525e13e226a030c28
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-27 18:29:18 +0100

    Add Layout component

---
+++ app/client/views/components/commons/Layout.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import colors from '../../constants/colors';
+
+const Layout = styled.div`
+  ${props =>
+    props.sidebar
+      ? `
+    background: ${colors.background};
+    min-height: 100vh;
+    flex-direction: row;
+    box-sizing: border-box;
+    flex: auto;
+    display: flex;
+    `
+      : `
+    background: ${colors.background};
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    flex: auto;
+    overflow-x: hidden;
+      `};
+`;
+
+export default Layout;
