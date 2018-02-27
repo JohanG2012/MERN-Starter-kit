fddf522298bae85d3d79805a7de02b589d594d41
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-27 18:24:34 +0100

    Add Menu component

---
+++ app/client/views/components/Menu/index.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import colors from '../../constants/colors';
+
+const Menu = styled.div`
+  color: rgba(255, 255, 255, 0.65);
+  background: ${colors.secondary};
+`;
+
+export default Menu;
