a6e6c3e251aa24f03a1e6fb21c1dc6e9af39e969
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-27 01:16:23 +0100

    Serve public files

--- app/server/server.js
+++ app/server/server.js
@@ -15,6 +15,17 @@ const db = require('./models');
 // Set port, init express
 const port = process.env.PORT || 8080;
 const app = express();
+app.use(express.static(`${__dirname}/public`));
+
+/*
+
+Allows CORS
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+}); */
 
 // Init bunyan logger
 const log = bunyan.createLogger(bunyanConfig);
