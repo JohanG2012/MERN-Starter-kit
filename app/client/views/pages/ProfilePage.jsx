176621620a475c593921df89462d10b94d2136df
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-27 19:11:31 +0100

    Add ProfilePage

---
+++ app/client/views/pages/ProfilePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Text from '../elements/Text';
+import DashboardBase from './DashboardBase';
+
+/**
+ * ProfilePage for the /user/profile route
+ */
+const ProfilePage = () => (
+  <DashboardBase>
+    <Text>Profile</Text>
+  </DashboardBase>
+);
+
+export default ProfilePage;
