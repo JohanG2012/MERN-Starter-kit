a0d2b56b590d1195dbae63e7f87a2154d91ec3b8
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-27 18:17:55 +0100

    Add colors constants

---
+++ app/client/views/constants/colors.js
@@ -0,0 +1,23 @@
+const colors = {
+  secondary: '#001529',
+  dark: '#000c17',
+  white: '#fff',
+  gray: '#f0f2f5',
+  background: '#f0f2f5',
+  primary: '#1890ff',
+  primaryShade: '#40a9ff',
+  secondaryShade: '#002140'
+};
+/*
+const colors = {
+  secondary: '#008975',
+  dark: '#009688',
+  white: '#fff',
+  background: '#f0f2f5',
+  gray: '#b2dfdb',
+  primary: '#00aa8d',
+  primaryShade: '#00Bf9a',
+  secondaryShade: '#002140'
+}; */
+
+export default colors;
