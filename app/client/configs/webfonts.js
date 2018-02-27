0ebb2d5c4edb6fd1d227daa80df11f83d40d3a93
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-27 11:59:25 +0100

    Add webfonts config

---
+++ app/client/configs/webfonts.js
@@ -0,0 +1,11 @@
+const webFonts = {
+  google: {
+    families: ['Poppins']
+  },
+  custom: {
+    families: ['FontAwesome'],
+    urls: ['https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css']
+  }
+};
+
+export default webFonts;
