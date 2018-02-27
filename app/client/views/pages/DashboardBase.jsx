b3cf32e4f14483848367945ee15e6dd63229042f
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-27 19:08:07 +0100

    Add DashboardBase Page

---
+++ app/client/views/pages/DashboardBase.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Layout from '../components/commons/Layout';
+import Main from '../elements/Main';
+import Footer from '../elements/Footer';
+import SidebarContainer from '../containers/SidebarContainer';
+import Header from '../elements/Header';
+import FlashMessage from '../components/FlashMessage';
+
+const defaultProps = {
+  children: 'page'
+};
+
+const propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
+};
+
+/**
+ * Used as Base for logged in Dashboard pages.
+ * @param {component} children Child components, which will be the content of the page.
+ */
+const DashboardBase = ({ children }) => (
+  <Layout sidebar>
+    <SidebarContainer />
+    <Layout>
+      <Header />
+      <Main>{children}</Main>
+      <Footer>MERN Boilerplate ©2018</Footer>
+    </Layout>
+  </Layout>
+);
+
+DashboardBase.propTypes = propTypes;
+DashboardBase.defaultProps = defaultProps;
+
+export default DashboardBase;
