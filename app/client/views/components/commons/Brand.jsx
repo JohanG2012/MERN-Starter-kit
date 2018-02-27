9e641fd35f2ca27e67eddbe70aea385a48122e9f
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-27 18:28:15 +0100

    Add Brand component

---
+++ app/client/views/components/commons/Brand.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import colors from '../../constants/colors';
+import fonts from '../../constants/fonts';
+
+const defaultProps = {
+  align: 'none'
+};
+
+const Brand = styled.div`
+  float: ${props => props.align};
+  color: ${colors.white};
+  height: 60px;
+  line-height: 60px;
+  text-align: center;
+  text-transform: uppercase;
+  font-family: ${fonts.primary};
+`;
+
+Brand.defaultProps = defaultProps;
+
+export default Brand;
