e9c07c82d0c845ff2448273d9b388eec65014566
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-27 18:43:37 +0100

    Add RegisterForm Container Component

---
+++ app/client/views/containers/RegisterFormContainer.jsx
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+
+import RegisterForm from '../components/RegisterForm';
+import { createFlash } from '../../state/layout/actions';
+import { registerPostData } from '../../state/register/actions';
+
+/* eslint-disable require-jsdoc */
+const mapDispatchToProps = dispatch => ({
+  sendForm: (url, fields) => dispatch(registerPostData(url, fields)),
+  showError: message => dispatch(createFlash('error', message)),
+  showSuccess: message => dispatch(createFlash('success', message))
+});
+
+const mapStateToProps = state => ({
+  loggedInAs: state.sessionReducer.loggedInAs,
+  errors: state.registerReducer.registerHasError.errorMessages,
+  success: state.registerReducer.registerPostDataSuccess
+});
+/* eslint-enable require-jsdoc */
+
+export default connect(mapStateToProps, mapDispatchToProps)(RegisterForm);
