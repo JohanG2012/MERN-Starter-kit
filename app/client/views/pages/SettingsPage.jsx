0d7f1b5884e413bd1c2dd55009e83fae3ef504b9
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-27 19:11:49 +0100

    Add SettingsPage

---
+++ app/client/views/pages/SettingsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Text from '../elements/Text';
+import DashboardBase from './DashboardBase';
+
+/**
+ * SettingsPage for the /user/settings route
+ */
+const SettingsPage = () => (
+  <DashboardBase>
+    <Text>Settings</Text>
+  </DashboardBase>
+);
+
+export default SettingsPage;
