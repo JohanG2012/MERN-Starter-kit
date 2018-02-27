bb7d276dcb7f0fd671af92d9cee4cbe565133804
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-02-27 18:16:07 +0100

    Add Main component

---
+++ app/client/views/elements/Main.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import colors from '../constants/colors';
+
+const Main = styled.main`
+  padding: 30px;
+  background: ${colors.white};
+  margin: 50px;
+`;
+
+export default Main;
